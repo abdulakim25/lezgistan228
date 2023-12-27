801bf55d00fb7dd71f538c88b3d38477d9672c92
abdulakim25 <149761328+abdulakim25@users.noreply.github.com>
2023-12-27 12:30:34 +0300

    Update lab_1.jl

--- lab_1.jl
+++ lab_1.jl
@@ -1,5 +1,5 @@
 function lab_1(r::Robot)   
-    for i ∈ (North, South, West, East)
+    for i ∈ (Nord, South, West, East)
         go_to_border_come_back_and_return_distance!(r, i; markers = true)
     end
     putmarker!(r)
